8d5e0657190053cb4ab7f6f3034784b4cf14686a
Steffen Ricklin <steffenricklin@gmail.com>
2021-06-10 10:08:08 +0200

    code cleanup ea.py. Added crossover.py

--- src/ea.py
+++ src/ea.py
@@ -9,15 +9,21 @@ import numpy as np
 from tqdm import trange
 import matplotlib.pyplot as plt
 # local imports
+from src.lsystem.crossover import cross_over
 from src.lsystem.utils import init_population
 plt.style.use('bmh')  # Use some nicer default colors
 
 
 class EA:
-    """
+    """Provides functions to run the evolutionary algorithm
     """
 
     def __init__(self, goal, population_params):
+        """Create the EA with the provided parameters and the to be achieved goal system.
+
+        :param goal: LSystem that is to be approximated by the EA
+        :param population_params: dictionary of parameters
+        """
         # self.goal_system = LSystem('A', {'B': 'BB', 'A': 'B[+AB-[A]--A][---A]'}, 22.5, 5)
         self.goal_system = goal  # L-System object
         self.goal_np = goal.to_numpy()  # L-System representation as numpy array
@@ -30,38 +36,33 @@ class EA:
         # init population
         self.population = init_population(self.pop_size, None, None, self.angle, self.nr_iter, rand=True)
 
-    def run_evolutions(self, n_gens, prob_mutation=0.75, tournament_size=2):
-        """starts of with a given L-system / candidate solution.
-        - evaluates the quality of each candidate
-        - then repeats until termination condition is satisfied:
-        - select candidate solutions for reproduction
-        - mutate the resulting candidates
-        - evaluate new candidates
-        - select candidates for the next generation
+    def run_evolutions(self, n_gens, prob_mutation=0.75, tournament_size=10):
+        """starts of with a the initial population and evolves it over n_gens generations.
+        - for n_gens
+            - select candidate solutions for reproduction
+            - mutate the resulting candidates
+            - evaluate new candidates
+            - select candidates for the next generation
+
+        :param n_gens: number of generations to simulate
+        :param prob_mutation: probability of a system to mutate its transformation rules
+        :param tournament_size: how many LSystems should compete with eachother
         """
 
         # simulate evolution
         fitness_population = None
         hu_season = True
         for _ in trange(n_gens, file=sys.stdout, desc='generations'):
-            # for gen in range(n_gens):
-            #     print("generation ", gen)
-            # ?select random pairs
-            # ?cross over
-            # Some plants can grow offspring from their own roots: cloning
-            # so who needs cross over anyway
-
-            # mutations
+            # make offsprings
             children = []
 
             pairs = np.random.choice(self.pop_size, (int(self.pop_size / 2), 2), replace=False)
-
             for pair in pairs:
                 parent_a = self.population[pair[0]]
                 parent_b = self.population[pair[1]]
                 pc = random.random()
                 if pc < self.cross_prob:
-                    self.cross_over(parent_a, parent_b, children)
+                    cross_over(parent_a, parent_b, children)
                 else:
                     child_a = copy.deepcopy(parent_a)
                     child_b = copy.deepcopy(parent_b)
@@ -70,6 +71,7 @@ class EA:
                     children.append(child_a)
                     children.append(child_b)
 
+            # mutate offsprings
             for _, child in enumerate(children):
                 child.mutate_transformations(prob_mutation)
             self.population.extend(children)
@@ -77,29 +79,35 @@ class EA:
             # select new generation
             if random.random() > 0.8:
                 hu_season = not hu_season
-            # print(hu_season)
+
             self.population, fitness_population = self.run_tournament_selection(self.population,
                                                                                 (self.goal_system, self.goal_np),
                                                                                 tournament_size,
                                                                                 self.pop_size)
-            # print("end selection")
-            children.clear()
+
+            children.clear()  # cleanup
         return self.population, fitness_population
 
     def run_tournament_selection(self, population, goal, tournament_size, size_pop, hu_season=True):
-        assert tournament_size <= size_pop
-        assert tournament_size > 1
         """
         Runs tournament selection on the current situation.
         This function computes the fitness of each candidate and then repeatedly
         chooses k candidates out of the population and selects the best one
         until the new population has formed. (selection with replacement)
+
+        :param population: list of LSystems to run the t.-selection with
+        :param goal: the to be achieved goal system: tuple(goal_system, goal_numpy)
+        :param tournament_size: how many winners to selected in the tournament
+        :param size_pop: how large the resulting population should be
+        :param hu_season: use hu moments fitness function if True, else use the fitness function in self.fitness_method
         """
+        assert tournament_size <= size_pop
+        assert tournament_size > 1
 
         selected = []
         fitness_selected = []
         # compute the fitness for each candidate
-        # print("start tournament selection")
+
         coords_list = []
         fitness_list = []
         for nr, system in enumerate(population):
@@ -126,50 +134,4 @@ class EA:
             selected.append(best_system)
             fitness_selected.append(best_fitness)
 
-        '''
-        # run several tournaments, until there are enough candidates selected to replace the population
-        while len(selected) < size_pop:
-        #    # choose some random candidates from the population
-            chosen_pop = random.sample(range(len(population)), tournament_size)
-
-            # custom argmin
-            f = lambda i: fitness_list[i]
-            winner_ind = min(chosen_pop, key=f)
-
-            # add the best solution
-            selected.append(population[winner_ind])
-            fitness_selected.append(fitness_list[winner_ind])
-        '''
-
         return selected, fitness_selected
-
-    def cross_over(self, parent_a, parent_b, children):
-        # find mathcing keys
-        rules_a = parent_a.get_transformations()
-        rules_b = parent_b.get_transformations()
-        matches = set(rules_a.keys()) & set(rules_b.keys())
-        if matches:
-            chosen_key = random.choice(list(matches))
-
-            # swap matching rules
-            child_a = copy.deepcopy(parent_a)
-            child_b = copy.deepcopy(parent_b)
-            child_a.replace_rule(chosen_key, rules_b[chosen_key])
-            child_b.replace_rule(chosen_key, rules_a[chosen_key])
-
-            # add children
-            children.append(child_a)
-            children.append(child_b)
-        else:
-            chosen_key_a = random.choice(list(rules_a.keys()))
-            chosen_key_b = random.choice(list(rules_b.keys()))
-
-            # swap matching rules
-            child_a = copy.deepcopy(parent_a)
-            child_b = copy.deepcopy(parent_b)
-            child_a.replace_rule(chosen_key_b, rules_b[chosen_key_b])
-            child_b.replace_rule(chosen_key_a, rules_a[chosen_key_a])
-
-            # add children
-            children.append(child_a)
-            children.append(child_b)
